37cb38e09d71ec3160b89b748b187fd276266596
Justin Alsing <j.a.alsing@gmail.com>
2020-04-21 11:58:53 +0200

    corrected typo in compile stan models

--- compile_stan_models.py
+++ compile_stan_models.py
@@ -24,12 +24,12 @@ f.close()
 
 model_vanilla_ar1_noseasonal = pystan.StanModel(model_code=dlm_vanilla_ar1_noseasonal)
 f = open('models/dlm_vanilla_ar1_noseasonal.pkl', 'wb')
-pickle.dump(dlm_vanilla_ar1_noseasonal, f)
+pickle.dump(model_vanilla_ar1_noseasonal, f)
 f.close()
 
 model_vanilla_ar2_noseasonal = pystan.StanModel(model_code=dlm_vanilla_ar2_noseasonal)
 f = open('models/dlm_vanilla_ar2_noseasonal.pkl', 'wb')
-pickle.dump(dlm_vanilla_ar2_noseasonal, f)
+pickle.dump(model_vanilla_ar2_noseasonal, f)
 f.close()
 
 
